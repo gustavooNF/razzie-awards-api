41047b383ec0436aac0926cce6bf8cee18db0ea4
Gustavo Faria <gustavoofaria@outlook.com>
2023-02-27 16:57:51 -0300

    Testes de integracao

---
+++ src/test/java/com/gustavo/texoit/razziesapi/RazzieAwardsApplicationTests.java
@@ -0,0 +1,36 @@
+package com.gustavo.texoit.razziesapi;
+
+import com.gustavo.texoit.razziesapi.application.RazzieAwardsIntervalResponse;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RazzieAwardsApplicationTests {
+
+    private static final String HTTP_LOCALHOST = "http://localhost:";
+    private static final String V_1_PRODUCERS = "/razzieawards-api/v1/producers";
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testRazziesInterval() {
+        ResponseEntity<RazzieAwardsIntervalResponse> responseEntity = restTemplate.getForEntity(HTTP_LOCALHOST + port + V_1_PRODUCERS + "/razzies", RazzieAwardsIntervalResponse.class);
+        Assert.assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+        Assert.assertNotNull(responseEntity.getBody());
+        Assert.assertNotNull(responseEntity.getBody().getMin());
+        Assert.assertNotNull(responseEntity.getBody().getMax());
+    }
+}
