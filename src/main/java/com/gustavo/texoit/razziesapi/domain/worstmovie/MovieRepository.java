73b94515125a3944bc04f7ae289735ab3c969806
Gustavo Faria <gustavoofaria@outlook.com>
2023-02-28 09:05:37 -0300

    Acrescentando a annotation @Repository

--- src/main/java/com/gustavo/texoit/razziesapi/domain/worstmovie/MovieRepository.java
+++ src/main/java/com/gustavo/texoit/razziesapi/domain/worstmovie/MovieRepository.java
@@ -1,9 +1,11 @@
 package com.gustavo.texoit.razziesapi.domain.worstmovie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     List<Movie> findByWinner(Boolean winner);
