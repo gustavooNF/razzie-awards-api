f6eb46f031ba8b960074b0c049df98a3009002bd
Gustavo Faria <gustavoofaria@outlook.com>
2023-02-28 10:46:44 -0300

    Incluindo descrição no end-point que retorna todos os produtores que venceram o premio

--- src/main/java/com/gustavo/texoit/razziesapi/domain/worstmovie/MovieRepository.java
+++ src/main/java/com/gustavo/texoit/razziesapi/domain/worstmovie/MovieRepository.java
@@ -1,5 +1,6 @@
 package com.gustavo.texoit.razziesapi.domain.worstmovie;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.List;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    @ApiResponse(description = "Busca todos os filmes vencedores")
     List<Movie> findByWinner(Boolean winner);
 }
